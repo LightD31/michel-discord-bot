6a70a93eaf662a702ad05293995e0927e60529ed
LightD31 <totodetoulouse@hotmail.fr>
2024-09-14 14:43:30 +0200

    fix: Corriger la méthode de récupération du canal et mettre à jour le nom de l'auteur dans le fichier JSON

--- extensions/temp.py
+++ extensions/temp.py
@@ -7,12 +7,12 @@ class Temp(Extension):
     @listen()
     async def on_ready(self):
         # Download all the message from the channel and make a json file with the content and the author
-        channel = await self.bot.get_channel(1282407006241820794)
+        channel = await self.bot.fetch_channel(1282407006241820794)
         messages = []
         async for message in channel.history(limit=100):
             messages.append({
                 'content': message.content,
-                'author': message.author.name
+                'author': message.author.username,
             })
         with open('messages.json', 'w') as f:
             json.dump(messages, f)
