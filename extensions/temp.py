143f6d903e87fdaa3881b1b5ee06147cab5b9fe4
LightD31 <totodetoulouse@hotmail.fr>
2024-09-14 14:18:02 +0200

    feat: Ajouter une extension Temp pour sauvegarder les messages d'un canal dans un fichier JSON

--- extensions/temp.py
+++ extensions/temp.py
@@ -1,2 +1,22 @@
-from interactions import Extension
-
+from interactions import Extension, Client, listen
+import json
+class Temp(Extension):
+    def __init__(self, bot):
+        self.bot : Client = bot
+    
+    @listen()
+    async def on_ready(self):
+        # Download all the message from the channel and make a json file with the content and the author
+        channel = await self.bot.get_channel(1282407006241820794)
+        messages = []
+        async for message in channel.history(limit=100):
+            messages.append({
+                'content': message.content,
+                'author': message.author.name
+            })
+        with open('messages.json', 'w') as f:
+            json.dump(messages, f)
+        print('Done')
+        
+        
+        
